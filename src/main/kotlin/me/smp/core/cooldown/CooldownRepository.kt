aceeaaa3cd8924136fb410ff05d66656bf3ad26a
ponktacology <ponktacology@mailfence.com>
2022-11-07 20:55:32 +0100

    make cooldown cache map concurrent

--- src/main/kotlin/me/smp/core/cooldown/CooldownRepository.kt
+++ src/main/kotlin/me/smp/core/cooldown/CooldownRepository.kt
@@ -11,14 +11,14 @@ import org.ktorm.dsl.eq
 import org.ktorm.entity.*
 import org.ktorm.support.postgresql.insertOrUpdate
 import java.util.*
-import kotlin.collections.HashMap
+import java.util.concurrent.ConcurrentHashMap
 
 class CooldownRepository : UUIDCache, KoinComponent {
 
     private val database: Database by inject()
     private val Database.cooldowns get() = this.sequenceOf(PlayerCooldowns)
-    private val cache = HashMap<UUID, MutableMap<String, Cooldown>>()
-    private val cooldowns = HashMap<String, CooldownType>()
+    private val cache = ConcurrentHashMap<UUID, MutableMap<String, Cooldown>>()
+    private val cooldowns = ConcurrentHashMap<String, CooldownType>()
 
     fun registerPersistentCooldown(cooldownType: CooldownType) = cooldowns.put(cooldownType.id, cooldownType)
 
@@ -29,8 +29,9 @@ class CooldownRepository : UUIDCache, KoinComponent {
     }
 
     fun reset(uuid: UUID, type: CooldownType) {
-        cache.computeIfAbsent(uuid) { HashMap() }
-            .computeIfAbsent(type.id) { Cooldown(Duration(type.duration)) }.reset()
+        if (!isOnCooldown(uuid, type)) return
+
+        setCooldown(uuid, type, System.currentTimeMillis())
 
         TaskDispatcher.dispatchAsync {
             database.insertOrUpdate(PlayerCooldowns) {
@@ -45,12 +46,16 @@ class CooldownRepository : UUIDCache, KoinComponent {
     override fun loadCache(uuid: UUID) {
         database.cooldowns.filter { it.player eq uuid }.forEach { cooldown ->
             val cooldownType = cooldowns[cooldown.id] ?: return@forEach
-            cache.computeIfAbsent(uuid) { HashMap() }
-                .computeIfAbsent(cooldownType.id) {
-                    Cooldown(Duration(cooldownType.duration), cooldown.resetAt)
-                }.reset()
+            setCooldown(uuid, cooldownType, cooldown.resetAt)
         }
 
+    }
+
+    private fun setCooldown(uuid: UUID, type: CooldownType, startedAt: Long) {
+        cache.computeIfAbsent(uuid) { ConcurrentHashMap() }
+            .computeIfAbsent(type.id) {
+                Cooldown(Duration(type.duration), startedAt)
+            }
     }
 
     override fun flushCache(uuid: UUID) {
